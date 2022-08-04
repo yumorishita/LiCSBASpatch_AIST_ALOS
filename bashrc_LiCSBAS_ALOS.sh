4df205ed5798ed4f74ec588ba9ed8a1c06b5ac95
yumorishita <asobu.morimori@gmail.com>
2022-08-04 22:01:14 +0900

    Create functions

---
+++ bashrc_LiCSBAS_ALOS.sh
@@ -0,0 +1,4 @@
+## Path
+export LICSBAS_ALOS_PATH="$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)"
+export PATH="$LICSBAS_ALOS_PATH/bin:$PATH"
+
---
+++ bin/copy_batch_LiCSBAS_ALOS.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo ""
+echo "Copy batch_LiCSBAS_ALOS.sh to the current directory."
+echo "(cp -i $LICSBAS_ALOS_PATH/batch_LiCSBAS_ALOS.sh .; chmod 755 batch_LiCSBAS_ALOS.sh)"
+cp -i $LICSBAS_ALOS_PATH/batch_LiCSBAS_ALOS.sh .
+chmod 755 batch_LiCSBAS_ALOS.sh
+
+echo ""
+echo "Edit batch_LiCSBAS_ALOS.sh, then run ./batch_LiCSBAS_ALOS.sh"
+echo ""
