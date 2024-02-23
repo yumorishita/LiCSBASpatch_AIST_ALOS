9ace64bc740738c934eb29644f722aec04da8668
yumorishita <asobu.morimori@gmail.com>
2024-02-23 21:30:04 +0900

    Add scripts for LiCSBAS2

---
+++ bin/copy_batch_LiCSBAS2_ALOS.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo ""
+echo "Copy batch_LiCSBAS2_ALOS.sh to the current directory."
+echo "(cp -i $LICSBAS_ALOS_PATH/batch_LiCSBAS2_ALOS.sh .; chmod 755 batch_LiCSBAS2_ALOS.sh)"
+cp -i $LICSBAS_ALOS_PATH/batch_LiCSBAS2_ALOS.sh .
+chmod 755 batch_LiCSBAS2_ALOS.sh
+
+echo ""
+echo "Edit batch_LiCSBAS2_ALOS.sh, then run ./batch_LiCSBAS2_ALOS.sh"
+echo ""
